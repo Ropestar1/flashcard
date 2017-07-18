4ebbd22f328670ac94ee901b0aa441f201bd2abd
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-17 23:21:25 -0700

    Working question generator

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,14 +1,14 @@
 var ClozeCard = function(fullText, cloze) {
   	if (this instanceof ClozeCard) {
-    	this.fullText = fullText;
-    	this.cloze = cloze;
-    	// can I put a conditional within a constructor????
-    	// if () {
-    	// 	this.partial =;
-    	// }
-    	// else {
-    	// 	console.log('Cloze is not present in full text.')
-    	// }
+    	if (fullText.includes(cloze)) {
+            this.fullText = fullText;
+            this.cloze = cloze;
+    		// this.partial = ;
+    	}
+    	else {
+    		console.log('Cloze is not present in full text.')
+            ClozeCard()
+    	}
 	}
 	else {
 		return new ClozeCard(fullText, cloze);
--- main-flashcard.js
+++ main-flashcard.js
@@ -5,57 +5,88 @@ var inquirer = require('inquirer');
 var request = require('request');
 
 var ClozeCard = require('./ClozeCard.js');
-var BasicCard = require ('./BasicCard.js');
+var BasicCard = require('./BasicCard.js');
 
 var strAnswerBasic = 'Answer basic card questions.';
 var strAnswerCloze = 'Answer cloze card questions.';
 var strCreateBasic = 'Create a basic flashcard.';
 var strCreateCloze = 'Create a cloze flashcard.';
 
+var count = 0;
+var correct = 0;
+
 //defined functions to be called during UX
-
-function creatingBasic() {
-	inquirer.prompt([
-	    // Here we create a basic text prompt.
-	    {
-			type: 'input',
-      		message: 'What title do you want for this basic flashcard?',
-     		name: 'titleForBasic'
-	    }
-	]).then(function(titleResponse) {
+function answerBasicCards() {
+	fs.readFile('./basic-cards.json', (err, data) => {
+		if (err) throw err;
+		// console.log(JSON.parse(data));
+		var parsedData = JSON.parse(data);
+		var keyArray = Object.keys(parsedData);
 		
-
-
-		//need to check if title exists in JSON file, if not proceed normally, if it does, ask to rename
-		fs.readFile('./basic-cards.json', (err, data) => {
-			if (err) throw err;
-			// console.log('Data result: ', JSON.parse(data));
-			// console.log('Data index 0: ', JSON.parse(data)['First President'])
-			// console.log('Object keys', Object.keys(JSON.parse(data)))
-			// console.log(JSON.parse(Object.keys(data["First President"])))
-			var titlesList = Object.keys(JSON.parse(data));
-
-			if (titlesList.indexOf(titleResponse.titleForBasic) === -1) {
-				basicInfo()
-			}
-			else {
-				console.log('The title you used already exists. Please use a different title.');
-				creatingBasic()
-			}
-
-		});
-		
-		// basicInfo()
-		// .then(function() {
-		// 	console.log('New basic card ', newBasicCard);		
-		// 	newBasicCard.prototype.basicTitle = titleResponse.titleForBasic;
-	
-		// })
-		//add prototype for the basic card title
+		console.log(keyArray)
+		// console.log(keyArray[])
+		if (count < keyArray.length) {
+			inquirer.prompt([
+		    	{
+					type: 'input',
+		      		message: parsedData[keyArray[count]].front,
+		     		name: 'userAnswer'
+			    }
+			]).then(function(basicInfoResponse) {
+				console.log(count);
+				console.log('array length', keyArray.length);
+				if (basicInfoResponse.userAnswer === parsedData[keyArray[count]].back) {
+					correct++;
+					count++;
+					console.log('Correct! You have ' + correct + ' answers correct.');
+					answerBasicCards()
+				}
+				else {
+					count++;
+					console.log('Wrong! You have ' + correct + ' answers correct.');
+					answerBasicCards()
+				}
+			});
+		}
+		//loop through questions and get input from inquirer
 	});
 }
 
-function basicInfo() {
+function answerClozeCards() {
+	fs.readFile('./cloze-cards.json', (err, data) => {
+		if (err) throw err;
+		var parsedData = JSON.parse(data);
+		var keyArray = Object.keys(parsedData);
+		
+		console.log(keyArray)
+		// console.log(keyArray[])
+		if (count < keyArray.length) {
+			inquirer.prompt([
+		    	{
+					type: 'input',
+		      		message: parsedData[keyArray[count]].partial,
+		     		name: 'userAnswer'
+			    }
+			]).then(function(clozeInfoResponse) {
+				console.log(count);
+				console.log('array length', keyArray.length);
+				if (clozeInfoResponse.userAnswer === parsedData[keyArray[count]].cloze) {
+					correct++;
+					count++;
+					console.log('Correct! You have ' + correct + ' answers correct.');
+					answerClozeCards()
+				}
+				else {
+					count++;
+					console.log('Wrong! You have ' + correct + ' answers correct.');
+					answerClozeCards()
+				}
+			});
+		}
+	});
+}
+
+function creatingBasic() {
 	inquirer.prompt([
     	{
 			type: 'input',
@@ -77,31 +108,43 @@ function basicInfo() {
 
 		var newBasicCard = BasicCard(frontText, backText);
 		console.log(newBasicCard)
-		// console.log(newBasicCard.hello) --> undefined
-		// return newBasicCard
+		
+		fs.readFile('/etc/passwd', (err, data) => {
+			if (err) throw err;
+			console.log(JSON.parse(data));
+		});
 	});
-}
 
+	// inquirer.prompt([
+	//     // Here we create a basic text prompt.
+	//     {
+	// 		type: 'input',
+ //      		message: 'What title do you want for this basic flashcard?',
+ //     		name: 'titleForBasic'
+	//     }
+	// ]).then(function(titleResponse) {
+	// 	//need to check if title exists in JSON file, if not proceed normally, if it does, ask to rename
+	// 	fs.readFile('./basic-cards.json', (err, data) => {
+	// 		if (err) throw err;
+	// 		// console.log('Data result: ', JSON.parse(data));
+	// 		// console.log('Data index 0: ', JSON.parse(data)['First President'])
+	// 		// console.log('Object keys', Object.keys(JSON.parse(data)))
+	// 		// console.log(JSON.parse(Object.keys(data["First President"])))
 
-
-function creatingCloze() {
-	inquirer.prompt([
-	    {
-			type: 'input',
-      		message: 'What title do you want for this cloze flashcard?',
-     		name: 'titleForCloze'
-	    }
-	]).then(function(titleResponse) {
-		//need to check if title exists, if not proceed normally, if it does, ask to rename
-		console.log('Adding information.');
-		clozeInfo();
-		//prototype the title for this object
-
-	});
+	// 	});
+		
+	// 	// basicInfo()
+	// 	// .then(function() {
+	// 	// 	console.log('New basic card ', newBasicCard);		
+	// 	// 	newBasicCard.prototype.basicTitle = titleResponse.titleForBasic;
+	
+	// 	// })
+	// 	//add prototype for the basic card title
+	// });
 }
 
 //adds the information to the clozeCard
-function clozeInfo() {
+function creatingCloze() {
 	inquirer.prompt([
     	{
 			type: 'input',
@@ -133,61 +176,33 @@ function clozeInfo() {
 	});
 }
 
-function answerBasicCards() {
-	console.log('Need to add reading capabilities here.')
-}
+function startUp() {
+	inquirer.prompt([
+	    { 
+			type: 'list',
+			message: 'What would you like to do?',
+			choices: [strAnswerBasic, strAnswerCloze, strCreateBasic, strCreateCloze],
+			name: 'action'
+	    }
+	]).then(function(inquirerResponse) {
+	    if (inquirerResponse.action === strAnswerBasic) {
+			console.log('Let\'s answer some basic card questions.');
+			answerBasicCards()
+	    }
+	    else if (inquirerResponse.action === strAnswerCloze) {
+			console.log('Let\'s answer some cloze card questions.');
+			answerClozeCards()
+	    }
+	    else if (inquirerResponse.action === strCreateBasic) {
+			console.log('Let\'s make a basic flashcard.')
+			creatingBasic();
+	    }
 
-function answerClozeCards() {
-	console.log('Need to add reading capabilities here.')
+	    else if (inquirerResponse.action === strCreateCloze) {
+			console.log('Let\'s make a cloze flashcard.')
+			creatingCloze();
+	    }
+	});
 }
-
 //CORE PROGRAM STARTING
-inquirer.prompt([
-    // Here we create a basic text prompt.
-    { 
-		type: 'list',
-		message: 'What would you like to do?',
-		choices: [strAnswerBasic, strAnswerCloze, strCreateBasic, strCreateCloze],
-		name: 'action'
-    }
-]).then(function(inquirerResponse) {
-    if (inquirerResponse.action === strAnswerBasic) {
-		console.log('Let\'s answer some basic card questions.')
-		//add function for answering basic cards
-    }
-    else if (inquirerResponse.action === strCreateCloze) {
-		console.log('Let\'s answer some cloze card questions.')
-		//add function for answering cloze cards
-    }
-    else if (inquirerResponse.action === strCreateBasic) {
-		console.log('Let\'s make a basic flashcard.')
-		creatingBasic();
-    }
-
-    else if (inquirerResponse.action === strCreateCloze) {
-		console.log('Let\'s make a cloze flashcard.')
-		creatingCloze();
-    }
-});
-
-
-
-// // TESTS
-// var firstBasicCard = new BasicCard('Who was the first president?', 'George Washington'); 
-// var firstClozeCard = new ClozeCard('George Washington was first president.', 'George Washington');
-
-// console.log('first basic front', firstBasicCard.front);
-// console.log('first basic back', firstBasicCard.back);
-
-// console.log('first cloze full text', firstClozeCard.fullText);
-// console.log('first cloze cloze', firstClozeCard.cloze);
-
-// var secondBasicCard = BasicCard('What is up?', 'The sky.'); 
-// var secondClozeCard = ClozeCard('The sky is up', 'sky');
-
-// console.log('second basic front', secondBasicCard.front);
-// console.log('second basic back', secondBasicCard.back);
-
-// console.log('second cloze full text', secondClozeCard.fullText);
-// console.log('second cloze cloze', secondClozeCard.cloze);
-// //END TESTS
+startUp();
